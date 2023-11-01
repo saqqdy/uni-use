89658119944c66ef4f38cb18825a557647b3156d
saqqdy <saqqdy@qq.com>
2023-11-01 14:24:40 +0800

    fix until type

--- packages/utils/until/src/index.ts
+++ packages/utils/until/src/index.ts
@@ -3,15 +3,17 @@ import { toValue } from '@uni-use/shared'
 import { UntilArray } from './UntilArray'
 import { UntilValue } from './UntilValue'
 
-export function util<T extends unknown[]>(r: WatchSource<T>): UntilArray<T>
-export function util<T, Not extends boolean = false>(r: WatchSource<T>, isNot?: Not): UntilValue<T>
-export default function util<T = any, Not extends boolean = false>(
+function util<T extends unknown[]>(r: WatchSource<T>): UntilArray<T>
+function util<T, Not extends boolean = false>(r: WatchSource<T>, isNot?: Not): UntilValue<T>
+function util<T = any, Not extends boolean = false>(
 	r: WatchSource<T>,
 	isNot?: Not
 ): UntilArray<T> | UntilValue<T> {
 	if (Array.isArray(toValue(r))) return new UntilArray<T>(r)
 	return new UntilValue<T>(r, isNot)
 }
+
+export default util
 
 export type * from './types'
 export type * from './UntilBase'
